ea84d06e3b94ef4d3608772101d0f9b8f834e096
neelamlab <150781336+neelamlab@users.noreply.github.com>
2024-01-05 19:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('base.html')
+
+
+@app.route('/people')
+def people():
+    return render_template('people.html')
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/alumni')
+def alumni():
+    return render_template('alumni.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
